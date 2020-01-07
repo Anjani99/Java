b5ac22fa6d2e33519ad845c181bc610895fdd7b3
Arjun Pinpoint <59592004+Arjun-Pinpoint@users.noreply.github.com>
2020-01-07 14:43:34 +0530

    Add files via upload

---
+++ BasicSyntax.java
@@ -0,0 +1,26 @@
+class BasicSyntax
+{
+	public static void main(String args[])
+	{
+		String color="red";
+		String name="dog";
+		int age=6;
+		
+		
+		System.out.println(color);
+		System.out.println(name);
+		System.out.println(age);
+		
+		
+		Sample obj=new Sample();
+		obj.eating();
+	}
+	
+	
+	public static void eating()
+	{
+		System.out.println("Dog is eating");
+	}
+}
+
+#youtube: youtube/Zooming
